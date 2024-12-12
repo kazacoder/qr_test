2b04f2be321d509e3e35433b3f52cb22d850b533
kazacoder <avtk.tlt@gmail.com>
2024-12-12 13:03:52 +0300

    test

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
-const scanner = new Html5QrcodeScanner('reader', {
-    qrbox: {
-        width: 250,
-        height: 250,
-    },
-    fps: 20,
-});
+// const scanner = new Html5QrcodeScanner('reader', {
+//     qrbox: {
+//         width: 250,
+//         height: 250,
+//     },
+//     fps: 20,
+// });
+//
+// scanner.render(success, error);
+//
+// function success(result) {
+//     console.log(result);
+//     document.getElementById('result').innerHTML = `
+//     <h2>Success!</h2>
+//     <p>${result}</p>
+//     `;
+//
+//     scanner.clear();
+//
+// }
+//
+// function error() {
+//
+// }
 
-scanner.render(success, error);
 
-function success(result) {
-    console.log(result);
-    document.getElementById('result').innerHTML = `
-    <h2>Success!</h2>
-    <p>${result}</p>
-    `;
+const scanner2 = new Html5Qrcode("reader")
+console.log(scanner2)
 
-    scanner.clear();
 
-}
+const qrConfig = { fps: 10, qrbox: { width: 300, height: 300 } };
+const html5QrCode = new Html5Qrcode("reader");
 
-function error() {
 
-}
+const handleClickAdvanced = () => {
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+        // props.onResult(decodedText);
+        handleStop();
+    };
+    html5QrCode.start(
+        {facingMode: "environment"},
+        qrConfig,
+        qrCodeSuccessCallback
+    );
+};
+
+const handleStop = () => {
+    try {
+        html5QrCode
+            .stop()
+            .then((res) => {
+                html5QrCode.clear();
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+document.getElementById('scan').addEventListener('click', handleClickAdvanced);
